19e711901e362c01f6e4c1b5b9c8557a756e89fc
François Lavieille <siempre14@hotmail.fr>
2018-05-12 20:39:11 +0200

    remove ZFC user

--- module/User/src/User/Controller/IndexController.php
+++ module/User/src/User/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace User\Controller;
 use User\Model\User;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Mvc\MvcEvent;
 
 class IndexController extends AbstractActionController
 {
@@ -23,6 +24,21 @@ class IndexController extends AbstractActionController
         $viewModel->setTemplate('user/index');
 
         return $viewModel;
+    }
+
+    protected function attachDefaultListeners()
+    {
+        parent::attachDefaultListeners();
+
+        $events = $this->getEventManager();
+        $events->attach('dispatch', [$this, 'preDispatch'], 100);
+    }
+
+    public function preDispatch(MvcEvent $e)
+    {
+        if (!$this->userModel->isConnected()) {
+            return $this->redirect()->toRoute('login');
+        }
     }
 
 }
